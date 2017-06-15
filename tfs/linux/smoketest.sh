449b8097a22cfb4865caeecabf9adb8eb10e86a0
isidor <inikolic@microsoft.com>
2017-06-15 10:31:27 +0200

    merge master

--- tfs/linux/smoketest.sh
+++ tfs/linux/smoketest.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
+set -e
 
 . ./scripts/env.sh
 . ./build/tfs/common/common.sh
 
 export ARCH="$1"
 export VSCODE_MIXIN_PASSWORD="$2"
-export AZURE_STORAGE_ACCESS_KEY="$3"
-export AZURE_STORAGE_ACCESS_KEY_2="$4"
-export MOONCAKE_STORAGE_ACCESS_KEY="$5"
-export AZURE_DOCUMENTDB_MASTERKEY="$6"
-export LINUX_REPO_PASSWORD="$7"
-VSO_PAT="$8"
+VSO_PAT="$3"
 
 echo "machine monacotools.visualstudio.com password $VSO_PAT" > ~/.netrc
 
@@ -29,8 +25,9 @@ step "Install distro dependencies" \
 step "Build minified" \
 	npm run gulp -- --max_old_space_size=4096 "vscode-linux-$ARCH-min"
 
-step "Run unit tests" \
-	./scripts/test.sh --xvfb --build --reporter dot
-
-step "Publish release" \
-	./build/tfs/linux/release.sh
+step "Run smoke test" \
+	pushd test/smoke
+	npm install
+	npm run compile
+	xvfb-run -a -s="-screen 0 1024x768x8" node src/main.js --latest "$AGENT_BUILDDIRECTORY/VSCode-linux-ia32/code-insiders"
+	popd
