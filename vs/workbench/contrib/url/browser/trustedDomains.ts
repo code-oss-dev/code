rmaz/vscode

Adds a timeout for trying to resolve remotes
This needs a better fix, but for remote file systems that require auth this can cause issues if auth requires opening a url

src/vs/workbench/contrib/url/browser/trustedDomains.ts
@@ -129,26 +129,29 @@ export async function configureOpenerTrustedDomainsHandler(
 
 async function getRemotes(fileService: IFileService, textFileService: ITextFileService, contextService: IWorkspaceContextService): Promise<string[]> {
 	const workspaceUris = contextService.getWorkspace().folders.map(folder => folder.uri);
-	const domains = await Promise.all<string[]>(workspaceUris.map(async workspaceUri => {
-		const path = workspaceUri.path;
-		const uri = workspaceUri.with({ path: `${path !== '/' ? path : ''}/.git/config` });
-		const exists = await fileService.exists(uri);
-		if (!exists) {
-			return [];
-		}
-		const content = (await (textFileService.read(uri, { acceptTextOnly: true }).catch(() => ({ value: '' })))).value;
-		const domains = new Set<string>();
-		let match: RegExpExecArray | null;
-
-		const RemoteMatcher = /^\s*url\s*=\s*(?:git@|https:\/\/)github\.com(?::|\/)([^.]*)\.git\s*$/mg;
-		while (match = RemoteMatcher.exec(content)) {
-			const repo = match[1];
-			if (repo) {
-				domains.add(`https://github.com/${repo}/`);
+	const domains = await Promise.race([
+		new Promise<string[][]>(resolve => setTimeout(() => resolve([]), 5000)),
+		Promise.all<string[]>(workspaceUris.map(async workspaceUri => {
+			const path = workspaceUri.path;
+			const uri = workspaceUri.with({ path: `${path !== '/' ? path : ''}/.git/config` });
+			const exists = await fileService.exists(uri);
+			if (!exists) {
+				return [];
 			}
-		}
-		return [...domains];
-	}));
+			const content = (await (textFileService.read(uri, { acceptTextOnly: true }).catch(() => ({ value: '' })))).value;
+			const domains = new Set<string>();
+			let match: RegExpExecArray | null;
+
+			const RemoteMatcher = /^\s*url\s*=\s*(?:git@|https:\/\/)github\.com(?::|\/)([^.]*)\.git\s*$/mg;
+			while (match = RemoteMatcher.exec(content)) {
+				const repo = match[1];
+				if (repo) {
+					domains.add(`https://github.com/${repo}/`);
+				}
+			}
+			return [...domains];
+		}))
+	]);
 
 	const set = domains.reduce((set, list) => list.reduce((set, item) => set.add(item), set), new Set<string>());
 	return [...set];

code-oss-dev/code

vs/workbench/contrib/url/browser/trustedDomains.ts
@@ -144,16 +144,18 @@ export function extractGitHubRemotesFromGitConfig(gitConfig: string): string[] {
 
 async function getRemotes(fileService: IFileService, textFileService: ITextFileService, contextService: IWorkspaceContextService): Promise<string[]> {
 	const workspaceUris = contextService.getWorkspace().folders.map(folder => folder.uri);
-	const domains = await Promise.all<string[]>(workspaceUris.map(async workspaceUri => {
-		const path = workspaceUri.path;
-		const uri = workspaceUri.with({ path: `${path !== '/' ? path : ''}/.git/config` });
-		const exists = await fileService.exists(uri);
-		if (!exists) {
-			return [];
-		}
-		const gitConfig = (await (textFileService.read(uri, { acceptTextOnly: true }).catch(() => ({ value: '' })))).value;
-		return extractGitHubRemotesFromGitConfig(gitConfig);
-	}));
+	const domains = await Promise.race([
+		new Promise<string[][]>(resolve => setTimeout(() => resolve([]), 2000)),
+		Promise.all<string[]>(workspaceUris.map(async workspaceUri => {
+			const path = workspaceUri.path;
+			const uri = workspaceUri.with({ path: `${path !== '/' ? path : ''}/.git/config` });
+			const exists = await fileService.exists(uri);
+			if (!exists) {
+				return [];
+			}
+			const gitConfig = (await (textFileService.read(uri, { acceptTextOnly: true }).catch(() => ({ value: '' })))).value;
+			return extractGitHubRemotesFromGitConfig(gitConfig);
+		}))]);
 
 	const set = domains.reduce((set, list) => list.reduce((set, item) => set.add(item), set), new Set<string>());
 	return [...set];